065f8a1e9b8704c8e9b56a038f4d761acaa49520
DevExpressExampleBot <fakeEmail>
2022-05-17 12:54:06 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,41 +1,41 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace HowToChangeCellColor
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-            Dim DataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim dataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
             Me.pivotGridField1 = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.fieldData = New DevExpress.XtraPivotGrid.PivotGridField()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
-            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.[Default]
             Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.pivotGridField1, Me.fieldData})
             Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
@@ -43,41 +43,41 @@ Namespace HowToChangeCellColor
             Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
             Me.pivotGridControl1.Size = New System.Drawing.Size(464, 288)
             Me.pivotGridControl1.TabIndex = 0
-            '
-            'pivotGridField1
-            '
+            AddHandler Me.pivotGridControl1.CustomDrawCell, New DevExpress.XtraPivotGrid.PivotCustomDrawCellEventHandler(AddressOf Me.pivotGridControl1_CustomDrawCell)
+            ' 
+            ' pivotGridField1
+            ' 
             Me.pivotGridField1.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
             Me.pivotGridField1.AreaIndex = 0
-            DataSourceColumnBinding1.ColumnName = "column"
-            Me.pivotGridField1.DataBinding = DataSourceColumnBinding1
+            dataSourceColumnBinding1.ColumnName = "column"
+            Me.pivotGridField1.DataBinding = dataSourceColumnBinding1
             Me.pivotGridField1.Name = "pivotGridField1"
-            '
-            'fieldData
-            '
+            ' 
+            ' fieldData
+            ' 
             Me.fieldData.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
             Me.fieldData.AreaIndex = 0
-            DataSourceColumnBinding2.ColumnName = "data"
-            Me.fieldData.DataBinding = DataSourceColumnBinding2
+            dataSourceColumnBinding2.ColumnName = "data"
+            Me.fieldData.DataBinding = dataSourceColumnBinding2
             Me.fieldData.Name = "fieldData"
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(464, 288)
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
 #End Region
+        Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
 
-        Private WithEvents pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private pivotGridField1 As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldData As DevExpress.XtraPivotGrid.PivotGridField
-	End Class
+        Private pivotGridField1 As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldData As DevExpress.XtraPivotGrid.PivotGridField
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,40 +1,32 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraPivotGrid
 
 Namespace HowToChangeCellColor
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
 
-			Dim table As New DataTable()
-			table.Columns.Add("column", GetType(String))
-			table.Columns.Add("data", GetType(Integer))
-			table.Columns.Add("hidden", GetType(Integer))
-			table.Rows.Add("col1", 1, 2)
-			table.Rows.Add("col2", 1, -2)
+    Public Partial Class Form1
+        Inherits Form
 
-			pivotGridControl1.DataSource = table
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            Dim table As DataTable = New DataTable()
+            table.Columns.Add("column", GetType(String))
+            table.Columns.Add("data", GetType(Integer))
+            table.Columns.Add("hidden", GetType(Integer))
+            table.Rows.Add("col1", 1, 2)
+            table.Rows.Add("col2", 1, -2)
+            pivotGridControl1.DataSource = table
+        End Sub
 
-		Private Sub pivotGridControl1_CustomDrawCell(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.PivotCustomDrawCellEventArgs) Handles pivotGridControl1.CustomDrawCell
-			If e.DataField Is fieldData Then
-				Dim ds As PivotDrillDownDataSource = e.CreateDrillDownDataSource()
-				Dim hiddenValue As Integer = CInt(Fix(ds.GetValue(0, "hidden")))
-				If hiddenValue > 0 Then
-					e.Appearance.BackColor = Color.Green
-				End If
-				If hiddenValue < 0 Then
-					e.Appearance.BackColor = Color.Red
-				End If
-			End If
-		End Sub
-	End Class
+        Private Sub pivotGridControl1_CustomDrawCell(ByVal sender As Object, ByVal e As PivotCustomDrawCellEventArgs)
+            If e.DataField Is fieldData Then
+                Dim ds As PivotDrillDownDataSource = e.CreateDrillDownDataSource()
+                Dim hiddenValue As Integer = CInt(ds.GetValue(0, "hidden"))
+                If hiddenValue > 0 Then e.Appearance.BackColor = Color.Green
+                If hiddenValue < 0 Then e.Appearance.BackColor = Color.Red
+            End If
+        End Sub
+    End Class
 End Namespace
